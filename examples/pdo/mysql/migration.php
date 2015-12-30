dea4b2a8ecc8c5a41cccfb079e4ef7f615e28d8c
Benjamin Michalski <benjamin.michalski@gmail.com>
2015-12-30 22:19:02 +0100

    Updates example.

--- examples/pdo/mysql/migration.php
+++ examples/pdo/mysql/migration.php
@@ -17,6 +17,7 @@ use Arlekin\Dbal\Driver\Pdo\MySql\Element\ColumnType;
 use Arlekin\Dbal\Driver\Pdo\MySql\Element\Schema;
 use Arlekin\Dbal\Driver\Pdo\MySql\Element\Table;
 use Arlekin\Dbal\Driver\Pdo\MySql\Migration\Builder\MigrationQueriesBuilder;
+use Arlekin\Dbal\Driver\Pdo\MySql\Migration\Builder\SchemaBuilder;
 use Arlekin\Dbal\Registry;
 
 $aTime = microtime(true);
@@ -49,15 +50,17 @@ $newSchema->addTable($table);
 
 $migrationQueriesBuilder = new MigrationQueriesBuilder();
 
-echo 'Creating migration queries.'.PHP_EOL;
-
 $migrationQueries = $migrationQueriesBuilder->getMigrationSqlQueries(new Schema(), $newSchema);
 
-echo 'Migration queries:'.PHP_EOL;
+echo 'Init migration queries:'.PHP_EOL;
 
-foreach ($migrationQueries as $query) {
-    echo '    '.$query.PHP_EOL;
-}
+$echoMigrationQueries = function ($migrationQueries) {
+    foreach ($migrationQueries as $query) {
+        echo '    '.$query.PHP_EOL;
+    }
+};
+
+$echoMigrationQueries($migrationQueries);
 
 $connection->connect();
 
@@ -72,15 +75,52 @@ $connection->dropAllDatabaseStructure();
 
 $connection->executeMultipleQueries($migrationQueries);
 
-echo 'Execute query: DESC Foo'.PHP_EOL;
+$descFoo = function () use ($connection) {
+    echo 'Execute query: DESC Foo'.PHP_EOL;
 
-$rs = $connection->executeQuery('DESC Foo');
+    $rs = $connection->executeQuery('DESC Foo');
 
-echo 'Result:'.PHP_EOL;
+    echo 'Result:'.PHP_EOL;
+    
+    foreach ($rs as $r) {
+        foreach ($r as $field => $value) {
+            echo '    '.$field.': '.$value.PHP_EOL;
+        }
+        echo '----'.PHP_EOL;
+    }
+};
 
-foreach ($rs[0] as $field => $value) {
-    echo '    '.$field.': '.$value.PHP_EOL;
-}
+$descFoo();
+
+$schemaBuilder = new SchemaBuilder();
+
+$schemaFromDb = $schemaBuilder->getFromDatabase($connection);
+
+//Adding a new column to the table
+$newColumn = new Column();
+
+$newColumn->setName(
+    'bar'
+)->setType(
+    ColumnType::TYPE_VARCHAR
+)->setParameter(
+    'length',
+    '255'
+)->setNullable(
+    false
+);
+
+$table->addColumn($newColumn);
+
+$newMigrationQueries = $migrationQueriesBuilder->getMigrationSqlQueries($schemaFromDb, $newSchema);
+
+echo 'New migration queries.'.PHP_EOL;
+
+$echoMigrationQueries($newMigrationQueries);
+
+$connection->executeMultipleQueries($newMigrationQueries);
+
+$descFoo();
 
 $connection->disconnect();
 
